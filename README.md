b24a78880938d58c35b3764a635d27d013d84658
Katherine Liu <katherine.z.liu@gmail.com>
2022-03-14 17:33:30 -0700

    readme fixed

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# cs147 accessibility2 chatty
+# CS 147 Accessible Design for Different Abilities – Chatty
+
+By Delali A., Carolina B., and Katherine L.
+
+## Setup
+ 
+Make sure you have Node installed first, with version 10.x or later. To check what version of Node you have, run
+```
+node -v
+```
+If you do not have Node, visit [here](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm) for installation instructions.
+
+Installing Expo:
+```
+npm install -g expo-cli
+```
+
+Running the app:
+```
+npm install
+expo start
+```
--- pages/Profile.js
+++ pages/Profile.js
@@ -24,49 +24,27 @@ export function Profile({ navigation }) {
     const [introductionText, setIntroductionText] = useState('');
     const [talkingPointsText, setTalkingPointsText] = useState('');
 
-  
-    // function Header() {
-    //     return (
-    //         <View style={styles.header_style}>
-    //             {/* working on adding settings icon */}
-    //             <Image source = {require("../assets/Icons/chatty.png")} style={styles.logo}/>
-    //         </View>
-    //     );
-    // }
+
     return (
         <>
         <View style={styles.container}>
-            {/* <View style={styles.header}>
-       
-                <Image source = {require("../assets/Icons/chatty.png")} style={styles.logo}/>
-        
-                <Image source = {require("../assets/Icons/chatty-settings-icon.png")} style={styles.logo}/>
-        
-             </View> */}
-
-
             <View style={styles.profile}>
-       
-                {/* <Image source = {require("../assets/Icons/profilepic.png")} style={styles.learnnative}/>
-                 */}
                 <View style={styles.left}>
                      <FontAwesome name="user-circle" size={100} color={Colors.chatty} />
-             </View>
-                <View>
-                    <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Jane</Text>
-                    <Text style={styles.username}>@jane10239842</Text>
                 </View>
+                <View stlye={styles.right}>
+                    <View style={styles.profileInfo}>
+                        <Text style={styles.name}>Jane</Text>
+                        <Text style={styles.username}>@jane10239842</Text>
+                    </View>
 
-                <Image source = {require("../assets/Icons/learning-native.png")} style={styles.learnnative}/>
-
+                    <Image source = {require("../assets/Icons/learning-native.png")} style={styles.learnnative}/>
                 </View>
             </View>
 
 
             <View>
-
-        {/* About + Logs clickable buttons  */}
+                {/* About + Logs clickable buttons  */}
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                     <Pressable onPress={() => setView('about')}>
                         <Text>About</Text>
@@ -77,59 +55,44 @@ export function Profile({ navigation }) {
                 </View>
             </View>
 
-             {/* Content based on which one is selected */}
-        {view === 'about' ? (
-          <View>
-            <Text>Introduction</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={setIntroductionText}
-              value={introductionText}
-              placeholder="Nothing yet"
-            />
-            <Text>Talking Points</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={setTalkingPointsText}
-              value={talkingPointsText}
-              placeholder="Nothing yet"
-            />
+            {/* Content based on which one is selected */}
+            {view === 'about' ? (
+            <View>
+                <Text>Introduction</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setIntroductionText}
+                    value={introductionText}
+                    placeholder="Nothing yet"
+                />
+                <Text>Talking Points</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTalkingPointsText}
+                    value={talkingPointsText}
+                    placeholder="Nothing yet"
+                />
 
-            <Text>Activity</Text>
-            <View style={styles.activity}>
-              {/* ? Image from Figma */}
-              {/* Lightbulb Image from Figma */}
-              {/* Blue people Image from Figma */}
-              <Image source = {require("../assets/Icons/activitybar.png")} style={styles.learnnative}/>
+                <Text>Activity</Text>
+                <View style={styles.activity}>
+                    {/* ? Image from Figma */}
+                    {/* Lightbulb Image from Figma */}
+                    {/* Blue people Image from Figma */}
+                    <Image source = {require("../assets/Icons/activitybar.png")} style={styles.learnnative}/>
 
+                </View>
             </View>
-          </View>
-        ) : (
-          <View style={styles.logs}>
-            {/* Logs image from Figma */}
-            <Text>Logs</Text>
-            <Image source = {require("../assets/Icons/logsicon.png")} style={styles.logimage}/>
+            ) : (
+                <View style={styles.logs}>
+                    {/* Logs image from Figma */}
+                    <Text>Logs</Text>
+                    <Image source = {require("../assets/Icons/logsicon.png")} style={styles.logimage}/>
 
-          </View>
-        )}
-      </View>
-
-
+                </View>
+            )}
+            </View>
 
         </>
-
-    //    // <>
-    //         {/* <View style={styles.left}>
-    //                 <FontAwesome name="user-circle" size={24} color={Colors.chatty} />
-    //         </View>
-    //         <View style={styles.container}>
-    //             <Text style={styles.name}>Jane</Text>
-    //             <Text style={styles.username}>@jane10239842</Text>
-    //             <Text style={styles.fillins}>Introduction</Text>
-    //             <Text style={styles.fillins}>Talking Points</Text>
-    //             <Text style={styles.fillins}>Activity</Text>
-    //         </View> */}
-    //   //  </>
     );
 }
   
@@ -143,48 +106,46 @@ const styles = StyleSheet.create({
         overflow: 'hidden',
         padding: 8,
     },
-
-    header: {
-        width: '100%',
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        marginBottom: 16,
-        borderBottomColor: 'gray',
-        paddingBottom: 8,
-        borderBottomWidth: 2,
-    },
-
     profile: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginHorizontal: 100,
+        paddingHorizontal: 30,
         marginBottom: 24,
+    },
+    left: {
+        flex: 1,
+    },
+    right: {
+        flex: 9,
+        paddingLeft: 10,
     },
     profileInfo: {
         flexDirection: 'row',
-    },
-
-    learnnative: {
-        flex: 1,
-        width: 100,
-        height: 100,
-        resizeMode: 'contain'
+        alignItems: "baseline",
     },
     name: {
-        color: 'black',
-       fontSize: 40,
-    },
-        
+        fontSize: 28,
+        fontFamily: "Lato-Bold",
+        paddingRight: 10,
+    }, 
     username: {
         color: Colors.purplegrey,
-        fontSize: 20,
+        fontSize: 16,
+    },
+    learnnative: {
+        flex: 1,
+        width: 200,
+        height: 200,
+        resizeMode: 'contain'
     },
 
     input: {
         height: 40,
         margin: 12,
         borderWidth: 1,
+        borderColor: Colors.lighter_purplegrey,
         padding: 10,
+        borderRadius: 25,
     },
 
     activity: {
